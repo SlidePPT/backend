b891d0becb74cfd19a2624b27fb3f5ecf74ef12e
태민 <48385263+UniTaeMin@users.noreply.github.com>
2019-11-15 01:00:45 +0900

    check save 구현

--- Controller/certification.js
+++ Controller/certification.js
@@ -22,10 +22,19 @@ exports.make = (req,res) => {
    )
 }
 
-exports.check = (req,res) => {
-    console.log("check")
-}
-
 exports.save = (req,res) => {
     console.log("save")
+    db.get('code')
+    .find({_id:req.body.ppt._id})
+    .assign({template:req.body.ppt.template,number:req.body.ppt.number})
+    .write()
+    console.log("성공적 저장")
+}
+
+exports.check = (req,res) => {
+    console.log("check")
+    let data = db.get('code')
+                .find({_id:req.body.code}) 
+                .value()
+    res.status(201).json(data)
 }
